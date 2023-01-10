5bf48a3ef233c23a7a0e70840949ee8faa08f1e1
Yaad Nahshon <ydnahshon@gmail.com>
2023-01-10 20:23:30 +0200

    Revert "fead: mysql database for Little Lemon"
    This reverts commit aefaec352a8187df65a19a4b1ff699c2dd164480.

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
-
-class Menu(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.SmallIntegerField()
-
-
-class Booking(models.Model):
-    name = models.CharField(max_length=255)
-    guests_num = models.SmallIntegerField()
-    booking_date = models.DateField()
+# Create your models here.
