da7809257cbea792cdfdee508139569c4a42c283
Yaad Nahshon <ydnahshon@gmail.com>
2023-01-12 11:30:07 +0200

    feat: add rest framework

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"tables", views.BookingViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("menu/", views.MenuItemsView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("booking/", include(router.urls)),
+    # path("booking/", views.BookingViewSet.as_view()),
 ]
